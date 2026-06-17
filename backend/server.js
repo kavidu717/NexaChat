65b765eaf3347249ba31cdcb0e58012e781cb8be
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-17 15:12:23 +0530

    feat:add the user routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 import http from 'http';
+import userRoutes from './src/routes/userRoutes.js';
 
 
 dotenv.config();
@@ -19,6 +20,8 @@ connectDB();
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use("/api/auth",userRoutes)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
