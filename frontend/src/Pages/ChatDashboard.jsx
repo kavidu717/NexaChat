8c26f610c0aa981875ffe934170f47995c6bd750
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-17 15:49:31 +0530

    feat:create the basic login register and chatdashboard page

--- frontend/src/Pages/ChatDashboard.jsx
+++ frontend/src/Pages/ChatDashboard.jsx
@@ -1,8 +1,7 @@
-export default function Login(){
+export default function ChatDashboard(){
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold">Login Page</h1>
+      <h1 className="text-2xl font-bold">Chat Dashboard</h1>
     </div>
   );
 };
-
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,7 +1,7 @@
-export default function Login(){
+export default function Register() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold">Login Page</h1>
+      <h1 className="text-2xl font-bold">Register Page</h1>
     </div>
   );
 };
