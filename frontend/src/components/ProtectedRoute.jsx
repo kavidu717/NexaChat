e6ebe2d5bee35f4cad6c5571f0669d971b6c9dee
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-17 16:39:58 +0530

    feat:create the public and protected routes file

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom";
+import {useAuthStore} from "../store/useAuthStore.js";
+import { Navigate } from "react-router-dom";
+
+
+const ProtectedRoute =()=>{
+
+    const authUser=useAuthStore((state)=>state.authUser);
+    return authUser ? <Outlet /> : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute
