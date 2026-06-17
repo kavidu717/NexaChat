f5660e9fbfb4217b327d5088ce0e8f230b9e2670
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-17 15:57:41 +0530

    feat:create the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { Routes } from 'react-router-dom'
+import Login from './Pages/Login'
+import { Route } from 'react-router-dom'
+import Register from './Pages/Register'
+import ChatDashboard from './Pages/ChatDashboard'
 
 function App() {
   
 
   return (
     <>
-     <h1>hello</h1>
+     <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<ChatDashboard />} />
+     </Routes>
     </>
   )
 }
