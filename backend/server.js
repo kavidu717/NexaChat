df8f0c3d92f618c8dbccd347711566709586f713
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-18 18:41:52 +0530

    feat: initialize socket.io server for real-time communication

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import http from 'http';
 import userRoutes from './src/routes/userRoutes.js';
 import authRoutes from './src/routes/authRoutes.js';
 import messageRoutes from './src/routes/messageRoutes.js';
+import { Server } from "socket.io";
 
 
 dotenv.config();
@@ -14,6 +15,20 @@ dotenv.config();
 const app = express();
 const server=http.createServer(app);
 
+const io=new Server(server, {
+        cors: {
+            origin: "http://localhost:3000",
+           methods: ["GET", "POST"],
+        },
+    });
+
+    io.on("connection", (socket) => {
+        console.log(`User connected: ${socket.id}`);
+    
+        socket.on("disconnect", () => {
+          console.log("User disconnected",socket.id);
+        });
+    })
 app.use(express.json());
 app.use(cors())
 
@@ -33,6 +48,6 @@ app.use("/api/messages",messageRoutes)
 
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`backend app listening on port ${port}`)
 })
