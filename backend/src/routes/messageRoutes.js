39f1002c9d601b9ea5f46419725a31d4e474c343
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-19 19:16:20 +0530

    feat: add isRead field and unread message count endpoints and functions

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/authmiddleware.js";
-import { getMessage, sendMessage } from "../controller/messageController.js";
+import { getMessage, sendMessage, getUnreadCounts, markMessagesAsRead } from "../controller/messageController.js";
 
 
 
 const router = express.Router();
+
+router.get("/unread", protect, getUnreadCounts);
+router.get("/mark-read/:senderId", protect, markMessagesAsRead);
 
 
 router.get("/:id", protect, getMessage);
